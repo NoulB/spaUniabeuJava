9d62d57d15e6db3a48b31a92a6ef60de7b4ccc9b
Felipe <noulbg@gmail.com>
2021-03-13 15:15:38 -0300

    First commit - spring boot

---
+++ src/main/java/com/psico/spauniabeu/SpauniabeuApplication.java
@@ -0,0 +1,13 @@
+package com.psico.spauniabeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpauniabeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpauniabeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psico/spauniabeu/SpauniabeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psico.spauniabeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpauniabeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
